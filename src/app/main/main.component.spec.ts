1ccfc2eb089f8afa3e3c44b99625fe69e4a13c92
Siddharth Ajmera <siddajmera@outlook.com>
2017-04-21 01:09:20 +0530

    Created a project using angular-cli and added files for the use of wallaby.js

---
+++ src/app/main/main.component.spec.ts
@@ -0,0 +1,8 @@
+import { MainComponent } from "./main.component";
+
+describe('Main Component', () => {
+    it('should be defined', () => {
+        let component = new MainComponent();
+        expect(component).toBeDefined();
+    });
+})
